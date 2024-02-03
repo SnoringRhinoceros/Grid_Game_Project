50ebfb0e4bf06759b49ee553403100fe14ebf442
David <davidshuschool@gmail.com>
2024-02-03 16:12:26 -0600

    Click on a cell and print the coordinates of the cell

--- src/main/java/com/example/generaltemplate/GameController.java
+++ src/main/java/com/example/generaltemplate/GameController.java
@@ -1,8 +1,11 @@
 package com.example.generaltemplate;
 
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 
@@ -11,14 +14,26 @@ public class GameController {
     public GridPane board;
     private Game game;
     private MyScreenController myScreenController;
+    private int selectedRow;
+    private int selectedCol;
 
     @FXML
     public void initialize() {
-        game = new Game();
+        EventHandler<ActionEvent> handleBoardClick = new EventHandler<>() {
+            @Override
+            public void handle(ActionEvent event) {
 
+                selectedRow = GridPane.getRowIndex((Button) event.getSource());
+                selectedCol = GridPane.getColumnIndex((Button) event.getSource());
+                System.out.println(selectedRow + "," + selectedCol);
+            }
+        };
+
+        game = new Game();
         for (int i = 0; i < game.getGrid().getButtons().length; i++) {
             for (int j = 0; j < game.getGrid().getButtons()[i].length; j++) {
                 board.add(game.getGrid().getButtons()[i][j], j, i);
+                game.getGrid().getButtons()[i][j].setOnAction(handleBoardClick);
             }
         }
 
